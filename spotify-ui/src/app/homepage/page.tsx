ae16f08aa2bc102721f2f606bde92058b8a68c1b
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2024-05-22 13:00:06 +0100

    working css file

--- spotify-ui/src/app/homepage/page.tsx
+++ spotify-ui/src/app/homepage/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export const Home = () => {
+const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className="w-full h-52 bg-red-500 flex justify-center">Home</div>
+    </div>
   )
 }
+
+
+export default Home;
--- spotify-ui/src/app/layout.tsx
+++ spotify-ui/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import './output.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
